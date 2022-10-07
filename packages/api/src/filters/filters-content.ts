091d68181e6066fcf2eb07f5a4c6d73a74728a82
jamesmorrispo <110367713+jamesmorrispo@users.noreply.github.com>
2022-10-07 18:13:17 -0500

    Create filters-content.ts

---
+++ packages/api/src/filters/filters-content.ts
@@ -0,0 +1,24 @@
+import { IpfsCommonContent, CommonContent } from '../types'
+import { ContentFilter } from './types'
+import { isEmptyObj, notDef } from '@subdeeply/util'
+
+type HasContent = {
+  content?: CommonContent | IpfsCommonContent
+}
+
+type Props<T> = ContentFilter & {
+  structs: T[],
+}
+
+export const isEmptyIpfsContent = (struct?: HasContent) => notDef(struct) || isEmptyObj(struct.content)
+
+export const notEmptyIpfsContent = (struct?: HasContent) => !isEmptyIpfsContent(struct)
+
+export function contentFilter<T extends HasContent> ({ structs, withContentOnly }: Props<T>): T[] {
+
+  if (withContentOnly) {
+    return structs.filter(notEmptyIpfsContent)
+  }
+
+  return structs
+}
