80f98b97b5e3a350ff5a7a968a9670a9a0a9764c
jamesmorrispo <110367713+jamesmorrispo@users.noreply.github.com>
2022-10-07 18:09:09 -0500

    Create index.ts
    set API for Subdeeply

---
+++ packages/api/src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * set API for Subdeeply.
+ *
+ * @packageDocumentation
+ *
+ * @category API
+ */
+
+export * from './subdeeply'
+export * from './substrate'
+export * from './ipfs'
+export * from './utils'
+export * from './connections'
